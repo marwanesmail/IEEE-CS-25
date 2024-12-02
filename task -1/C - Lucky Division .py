464a837cdb7c8fe527491b759a6a94fa2c0557f1
marwan <153552163+marwanesmail@users.noreply.github.com>
2024-12-02 11:45:04 +0200

    Update C - Lucky Division .py

--- task -1/C - Lucky Division .py
+++ task -1/C - Lucky Division .py
@@ -1,18 +1,1 @@
-def is_lucky(number):
-    for digit in str(number):
-        if digit != '4' and digit != '7':
-            return False
-    return True
 
-def is_almost_lucky(n):
-    for i in range(1, n + 1):
-        if is_lucky(i) and n % i == 0:
-            return True
-    return False
-
-n = int(input("int num : "))
-
-if is_almost_lucky(n):
-    print("YES")
-else:
-    print("NO")
